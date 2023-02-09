07198063740dede06e5648741a0ff8c3421044c7
Mohamed Gabr <mohamed.gabr@rossvideo.com>
2023-02-08 23:33:12 -0500

    configure ORM service

--- nest-demo/src/app.module.ts
+++ nest-demo/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule, UserModule, BookmarkModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
