e04ccec2857635e3c8622cbab349e091e12e5be8
Mohamed Gabr <mohamed.gabr@rossvideo.com>
2023-02-10 02:17:28 -0500

    argon-cryptographic

--- nest-demo/src/auth/auth.service.ts
+++ nest-demo/src/auth/auth.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from 'src/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService{
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
 
-    constructor(private prisma:PrismaService){}
-    signup(){
-    return { msg: 'I am sign up' }
+    //generate a password
+    const hash = await argon.hash(dto.password);
+    //save a new user
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      }
+    })
+    return user;
     }
 
     signin(){
--- nest-demo/src/dto/auth.dto.ts
+++ nest-demo/src/dto/auth.dto.ts
@@ -1,9 +1,11 @@
-import { isEmail, isNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto {
 
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
-  @IsString()
+  @IsNotEmpty()
   password: string;
 }
